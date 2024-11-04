0580a70ca5781b998ed30cb6aaaa5b545031a587
4moree <46574507+4moree@users.noreply.github.com>
2024-11-04 16:02:46 +0300

    Fixes after code review

--- property/admin.py
+++ property/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from .models import Flat, Complaint, Owner, PropertyOwnerOwnedFlats
+from .models import Flat, Complaint, Owner
 
 
 class PropertyOwnerInline(admin.TabularInline):
-    model = PropertyOwnerOwnedFlats
+    model = Owner.flats.through
     extra = 1
     raw_id_fields = ('owner',)
 
-
+@admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'address', 'owner']
     readonly_fields = ['created_at']
@@ -15,17 +15,15 @@ class FlatAdmin(admin.ModelAdmin):
     list_editable = ['new_building']
     list_filter = ['new_building', 'has_balcony', 'rooms_number']
     raw_id_fields = ['liked_by']
-
     inlines = [PropertyOwnerInline]
 
+@admin.register(Complaint)
 class ComplaintAdmin(admin.ModelAdmin):
     list_display = ['user', 'flat', 'text']
     raw_id_fields = ['flat']
 
+@admin.register(Owner)
 class OwnerAdmin(admin.ModelAdmin):
-    list_display = ('user', 'owner_pure_phone')
+    list_display = ('user', 'pure_phone')
 
 
-admin.site.register(Flat, FlatAdmin)
-admin.site.register(Complaint, ComplaintAdmin)
-admin.site.register(Owner, OwnerAdmin)
--- property/models.py
+++ property/models.py
@@ -5,9 +5,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200, blank=True)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20, blank=True)
-    owner_pure_phone = PhoneNumberField('Нормализированный номер владельца',region="RU", blank=True)
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
@@ -50,7 +47,7 @@ class Flat(models.Model):
         blank=True,
         db_index=True)
     new_building = models.BooleanField('Новостройка', blank=True, null=True)
-    liked_by = models.ManyToManyField(User, verbose_name="Кто лайкнул", blank=True)
+    liked_by = models.ManyToManyField(User, verbose_name="Кто лайкнул",related_name='liked_flats', blank=True)
 
 
 
@@ -58,19 +55,13 @@ class Flat(models.Model):
         return f'{self.town}, {self.address} ({self.price}р.)'
 
 class Complaint(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Кто жаловался')
-    flat = models.ForeignKey(Flat, on_delete=models.CASCADE, verbose_name='Квартира на которую пожаловались')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Кто жаловался', related_name='complaints')
+    flat = models.ForeignKey(Flat, on_delete=models.CASCADE, verbose_name='Квартира на которую пожаловались', related_name='complaints')
     text = models.TextField(verbose_name='Текст жалобы')
 
 class Owner(models.Model):
     user = models.CharField('ФИО Владельца', max_length=200)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
-    owner_pure_phone = PhoneNumberField('Нормализированный номер владельца', region="RU", blank=True)
+    phonenumber = models.CharField('Номер владельца', max_length=20)
+    pure_phone = PhoneNumberField('Нормализированный номер владельца', region="RU", blank=True)
     flats = models.ManyToManyField (Flat, related_name='owners', verbose_name='Квартиры в собственности', blank=True)
 
-class PropertyOwnerOwnedFlats(models.Model):
-    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
-    flat = models.ForeignKey(Flat, on_delete=models.CASCADE)
-
-    class Meta:
-        unique_together = (('owner', 'flat'),)
